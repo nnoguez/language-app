10022ac12ccf2df65d51524ccdc3cefdf4dfb335
MykeriaC <35880700+MykeriaC@users.noreply.github.com>
2022-05-12 02:39:08 -0400

    May 12, 2022 2:39 AM

--- language-app/App.js
+++ language-app/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {Card, Button} from 'react-native-elements';
+import { color } from 'react-native-elements/dist/helpers';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Card>Open up Ap.js to start working on your app!</Card>
-      <Button title="Click Me!"></Button>
+      <Button 
+        title="Fuck Me!"
+      ></Button>
       <StatusBar style="auto" />
     </View>
 
@@ -16,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'salmon',
     alignItems: 'center',
     justifyContent: 'center',
   },
